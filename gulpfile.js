ab836d4405671666d4cdd859cd604f77bc25552d
Elegantcodes <info@elegantcodes.com>
2015-04-15 07:42:56 +0200

    Added Browserfiy

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee'),
-	concat = require('gulp-concat');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat');
 
-var cofeeSources = ['components/coffee/tagline.coffee'];
+var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
 'components/scripts/rclick.js',
 'components/scripts/pixgrid.js',
@@ -12,14 +13,15 @@ var jsSources = [
 ];
 
 gulp.task('coffee', function() {
-	gulp.src(cofeeSources)
-		.pipe(coffee({ bare: true })
-			.on('error', gutil.log))
-		.pipe(gulp.dest('components/scripts'))
+  gulp.src(coffeeSources)
+    .pipe(coffee({ bare: true })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
 });
 
 gulp.task('js', function() {
-	gulp.src(jsSources)
-		.pipe(concat('script.js'))
-		.pipe(gulp.dest('builds/development/js'))
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+	.pipe(gulp.dest('builds/development/js'))
 });
