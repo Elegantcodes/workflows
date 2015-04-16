f5970d333c88e73784f2d618c7d0c832540a35b0
Elegantcodes <info@elegantcodes.com>
2015-04-16 03:30:33 +0200

    Created Default Task

--- gulpfile.js
+++ gulpfile.js
@@ -33,8 +33,10 @@ gulp.task('compass', function() {
     .pipe(compass({
         sass: 'components/sass',
         image: 'builds/development/images',
-        style: 'expanded'
+        style: 'expanded',
     })
     .on('error', gutil.log))
     .pipe(gulp.dest('builds/development/css'))
 });
+
+gulp.task('default', ['coffee', 'js', 'compass']);
